b78f6bf28c957b4f64ceb451313ac56ede2f10a0
Sam Fonseca <samfonseca@utexas.edu>
2014-10-08 21:06:43 -0500

    Added .travis.yml

--- Quandl/__init__.py
+++ Quandl/__init__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
 __authors__ = (
+    'Sam Fonseca',
     'Mark Hartney',
     'Chris Stevens',
 )
-__maintainer__ = 'Chris Stevens'
+__maintainer__ = 'Sam Fonseca'
 __email__ = 'connect@quandl.com'
 __url__ = 'http://www.quandl.com/'
 __license__ = 'MIT'
